afe97764def6ae4c525fd789f5cc96e6bcfac137
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-17 05:47:53 -0700

    feat: Activity 9 in DSA
    concat(), includes(), push(), unshift(), pop(), and shift() methods in JavaScript

---
+++ script.js
@@ -0,0 +1,33 @@
+//Activity 9
+//Fill in the missing component of the given code:
+
+// concat()
+const ARR1 = ["Cecile", "Lone"];
+const ARR2 = ["Emil", "Tobias", "Linus"];
+const CHILDREN = ARR1.concat(ARR2);
+console.log(CHILDREN);
+
+// includes()
+let text = "Hello world, welcome to the universe.";
+let result = text.includes("world");
+console.log(result);
+
+// push()
+const FRUITS = ["Banana", "Orange", "Apple", "Mango"];
+FRUITS.push("Kiwi");
+console.log(FRUITS);
+
+// unshift()
+const ARRAY1 = [1, 2, 3];
+console.log(ARRAY1.unshift(4, 5));
+console.log(ARRAY1);
+
+// pop()
+FRUITS.pop();
+console.log(FRUITS);
+
+// shift
+const ARRAY2 = [1, 2, 3];
+const FIRST_ELEMENT = ARRAY2.shift();
+console.log(ARRAY2);
+console.log(FIRST_ELEMENT);
